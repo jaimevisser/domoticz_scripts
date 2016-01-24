67e11e527acd5a0bf5fbfa373bf8b80d9a82305d
Jaime Visser <jaimevisser@gmail.com>
2016-01-24 20:47:54 +0100

    Fix loop variable, debugging prints.

--- script_time_pir.lua
+++ script_time_pir.lua
@@ -8,7 +8,7 @@ utils = require('utils')
 for device, mapping in pairs(mappings.map) do
     local lastupdate = mapping.timeout * 2
 
-    for detector in ipairs(mapping) do
+    for i, detector in ipairs(mapping) do
         if otherdevices[detector] ~= nil then
             lastupdate = min(lastupdate, utils.timedifference(otherdevices_lastupdate[detector]))
         else
@@ -16,8 +16,11 @@ for device, mapping in pairs(mappings.map) do
         end
     end
 
+    print("DEBUG : last timout for '"..device.."' was "..tostring(lastupdate).." seconds ago")
+
     if (lastupdate > mapping.timeout and lastupdate < (mapping.timeout + 60)) then
         commandArray[device] = 'Off'
+        print(device.." : Timeout reached, turning off.")
     end
 end
 
