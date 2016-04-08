8fb76dd426afc92bde3b299f6b1c5da634c73ddb
Jaime Visser <jaimevisser@gmail.com>
2016-04-08 20:15:35 +0200

    fix typo!

--- script_device_bijkeuken.lua
+++ script_device_bijkeuken.lua
@@ -4,7 +4,7 @@ for deviceName, deviceValue in pairs(devicechanged) do
     if (deviceName == 'Bijkeuken Deursensor') then
         print("Device based event fired on '" .. deviceName .. "', value '" .. tostring(deviceValue) .. "'")
         print("SUN: " .. otherdevices_svalues['Weer Centrum - Zon'])
-        local sunvalue = toNumber(otherdevices_svalues['Weer Centrum - Zon'])
+        local sunvalue = tonumber(otherdevices_svalues['Weer Centrum - Zon'])
         if (deviceValue == "On" and sunvalue < 200) then
             commandArray['Bijkeuken Licht'] = "On" -- "On" / "Off"
         elseif (deviceValue == "Off") then
