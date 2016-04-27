5de7eec07580f0b325bdb81352fc02ec938172ef
Jaime Visser <jaimevisser@gmail.com>
2016-04-27 12:48:34 +0200

    Nightly reboot

--- script_time_reboot.lua
+++ script_time_reboot.lua
@@ -1,5 +1,7 @@
 -- Reboot at midnight
 
+commandArray = {}
+
 time = os.date("*t")
 print(tostring(time))
 if(time.hour == 12 and time.minute == 45) then
@@ -7,4 +9,4 @@ if(time.hour == 12 and time.minute == 45) then
     os.execute("sudo reboot")
 end
 
-return {}
+return commandArray
