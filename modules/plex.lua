fd5f7820da83af16d6db3398c739168c49f41ee8
Jaime Visser <jaimevisser@gmail.com>
2016-07-12 21:41:58 +0200

    plex integration

--- modules/plex.lua
+++ modules/plex.lua
@@ -8,6 +8,7 @@ local data = utils.getURL(plexurl .. 'status/sessions/?X-Plex-Token=' .. plextok
 
 string.gsub(data, '<Player.-\/>', function(s)
     local c, s = string.match(s, 'platform="(.-)" .* state="(.-)"')
+    log(c..":"..s)
     clients[c] = { status = s }
 end)
 
--- script_time_script.lua
+++ script_time_script.lua
@@ -2,6 +2,7 @@ package.path = package.path .. ';' .. '/home/pi/domoticz/scripts/lua/?.lua'
 utils = require('modules/utils')
 require('modules/devices')
 require('modules/time')
+Plex = require('modules/plex')
 
 commandArray = {}
 
