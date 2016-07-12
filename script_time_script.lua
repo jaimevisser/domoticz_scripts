b9dd5a35cb702f9dc386af0d0cd550c8f729ef12
Jaime Visser <jaimevisser@gmail.com>
2016-07-12 21:53:32 +0200

    stop working on plex for a bit

--- script_time_script.lua
+++ script_time_script.lua
@@ -2,7 +2,7 @@ package.path = package.path .. ';' .. '/home/pi/domoticz/scripts/lua/?.lua'
 utils = require('modules/utils')
 require('modules/devices')
 require('modules/time')
-Plex = require('modules/plex')
+-- Plex = require('modules/plex')
 
 commandArray = {}
 
