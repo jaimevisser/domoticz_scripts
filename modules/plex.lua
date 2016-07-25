ab2037ec0246842afda8973ae4a29c4f57145ed6
Jaime Visser <jaimevisser@gmail.com>
2016-07-25 20:53:44 +0200

    Change capitalization of plex status check

--- modules/plex.lua
+++ modules/plex.lua
@@ -13,7 +13,7 @@ string.gsub(data, '<Player.-/>', function(s)
 end)
 
 
-if (clients["Chromecast"] ~= nil and clients["Chromecast"].status == "Playing") then
+if (clients["Chromecast"] ~= nil and clients["Chromecast"].status == "playing") then
     PlexTV.turnOn()
 else
     PlexTV.turnOff()
