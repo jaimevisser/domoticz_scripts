0c0acb5616f886e11ff439a12ae273ab632cecdc
Jaime Visser <jaimevisser@gmail.com>
2016-07-25 20:25:21 +0200

    Capture output of curl

--- modules/utils.lua
+++ modules/utils.lua
@@ -13,8 +13,19 @@ function M.timedifference(s)
     return difference
 end
 
+function M.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+
 function M.getURL(url)
-    return os.capture("curl " .. url)
+    return M.capture("curl " .. url, false)
 end
 
 return M
