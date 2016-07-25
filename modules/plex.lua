cfc4465da9df98e14dafe236ca5f08b2e9a00bfc
Jaime Visser <jaimevisser@gmail.com>
2016-07-25 20:14:45 +0200

    Another try at plex

--- modules/plex.lua
+++ modules/plex.lua
@@ -2,7 +2,11 @@ local plexurl = 'http://192.168.1.10:32400/'
 local plextoken = '8f8a23640278ed745414aa63b776d20aeafd149a'
 local clients = {}
 
+scriptname = "T.PLEX"
+
 local data = utils.getURL(plexurl .. 'status/sessions/?X-Plex-Token=' .. plextoken)
+
+log(data)
 
 string.gsub(data, '<Player.-/>', function(s)
     local c, s = string.match(s, 'platform="(.-)" .* state="(.-)"')
