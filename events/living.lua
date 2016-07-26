d780c4d268c92b236981a86f57d64233ceb360f3
Jaime Visser <jaimevisser@gmail.com>
2016-07-26 21:26:49 +0200

    Check light change

--- events/living.lua
+++ events/living.lua
@@ -17,10 +17,13 @@ living_detector.whenOn(function()
     end
 end)
 
-if (PlexTV.changed and living_lights.on) then
-    if (PlexTV.on) then
-        Switch("Scene:TV kijken").turnOn()
-    else
-        Switch("Scene:Woonkamer aan").turnOn()
+if (PlexTV.changed) then
+    log("Plex changed!")
+    if (living_lights.on) then
+        if (PlexTV.on) then
+            Switch("Scene:TV kijken").turnOn()
+        else
+            Switch("Scene:Woonkamer aan").turnOn()
+        end
     end
 end
--- timed/living.lua
+++ timed/living.lua
@@ -10,10 +10,13 @@ if (detector.off and round_minutes(detector.lastupdate - timeout) == 0) then
     Switch("Keuken Spots").turnOff()
 end
 
-if (PlexTV.changed and living_lights.on) then
-    if (PlexTV.on) then
-        Switch("Scene:TV kijken").turnOn()
-    else
-        Switch("Scene:Woonkamer aan").turnOn()
+if (PlexTV.changed) then
+    log("Plex changed!")
+    if (living_lights.on) then
+        if (PlexTV.on) then
+            Switch("Scene:TV kijken").turnOn()
+        else
+            Switch("Scene:Woonkamer aan").turnOn()
+        end
     end
 end
