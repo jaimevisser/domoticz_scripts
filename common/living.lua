04ea68430e33ab48ac623314c46c4141d7978b53
Jaime Visser <jaimevisser@gmail.com>
2016-07-30 17:32:03 +0200

    Light based switching (first attempt)

--- common/living.lua
+++ common/living.lua
@@ -1,6 +1,19 @@
 scriptname = "C.LIVING"
 
+local kitchen_detector = Switch("Keuken detector aanrecht")
+local living_detector = Switch("Woonkamer Sensor")
+local kitchenette_door = Switch("Bijkeuken Deursensor")
+local lux = Sensor("Woonkamer Lux")
 local living_lights = Dimmer("Zithoek Licht")
+
+Living = {}
+Living.timeout = minutes(90)
+Living.presence = kitchen_detector.on or living_detector.on or kitchenette_door.on
+Living.presence = Living.presence or (kitchen_detector.lastupdate < Living.timeout)
+Living.presence = Living.presence or (living_detector.lastupdate < Living.timeout)
+Living.presence = Living.presence or (living_detector.lastupdate < Living.timeout)
+Living.dark = lux.value[1] < 50
+Living.bright = lux.value[1] > 100
 
 if (PlexTV.changed) then
     log("Plex changed!")
--- events/living.lua
+++ events/living.lua
@@ -4,7 +4,6 @@ local kitchen_detector = Switch("Keuken detector aanrecht")
 local living_detector = Switch("Woonkamer Sensor")
 local lux = Sensor("Woonkamer Lux")
 
-
 kitchen_detector.whenOn(function()
     log("presence detected in kitchen")
     Switch("Keuken Spots").turnOn()
@@ -12,8 +11,26 @@ end)
 
 living_detector.whenOn(function()
     log("presence detected in living room")
-    if (PlexTV.off and lux.value[1] < 20) then
-        Switch("Scene:Woonkamer aan").turnOn()
+    if (Living.dark) then
+        if (PlexTV.on) then
+            Switch("Scene:TV kijken").turnOn()
+        else
+            Switch("Scene:Woonkamer aan").turnOn()
+        end
     end
 end)
 
+lux.whenChanged(function()
+    if (Living.presence) then
+        if (Living.dark) then
+            if (PlexTV.on) then
+                Switch("Scene:TV kijken").turnOn()
+            else
+                Switch("Scene:Woonkamer aan").turnOn()
+            end
+        end
+        if (Living.bright) then
+            Switch("Scene:Woonkamer uit").turnOn()
+        end
+    end
+end)
