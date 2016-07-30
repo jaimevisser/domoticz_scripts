c402499293699131d474c498ea63e5c4c2f30f12
Jaime Visser <jaimevisser@gmail.com>
2016-07-30 22:59:54 +0200

    Small cleanup

--- timed/living.lua
+++ timed/living.lua
@@ -3,12 +3,10 @@ scriptname = "T.LIVING"
 if (Living.detector.kitchen.off and
         Living.detector.kitchen.lastupdate == Living.timeout_kitchen) then
     log("nobody in the kitchen for " .. Living.timeout_kitchen .. "s")
-
     Living.light.kitchen_spots.turnOff()
 end
 
 if (Living.lights.on and Living.lastupdate == Living.timeout) then
     log("nobody in the living for " .. Living.timeout .. "s")
-
     Living.lights.turnOff()
 end
